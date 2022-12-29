3c94f0682c44283247e8ee7df33c053d9e6e842a
Hiieu <you@example.com>
2022-12-29 15:51:17 +0000

    change ownership

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'A dev blog by Hieu Doan',
+  author: 'Hieu Doan',
+  headerTitle: 'Dev Blog',
+  description: '"Join me on my developer journey as I document the problems I face and the solutions I find. By sharing my experiences, I hope to help others."',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
