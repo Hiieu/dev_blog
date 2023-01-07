537261ec1b01cde90539910e212626578e9c3c3d
Hiieu <you@example.com>
2023-01-07 18:19:28 +0000

    remove prod thingy

--- components/analytics/index.tsx
+++ components/analytics/index.tsx
@@ -21,7 +21,7 @@ const Analytics = () => {
       {isProduction && siteMetadata.analytics.plausibleDataDomain && <Plausible />}
       {isProduction && siteMetadata.analytics.simpleAnalytics && <SimpleAnalytics />}
       {isProduction && siteMetadata.analytics.umamiWebsiteId && <Umami />}
-      {isProduction && siteMetadata.analytics.googleAnalyticsId && <GA />}
+      {siteMetadata.analytics.googleAnalyticsId && <GA />}
     </>
   )
 }
